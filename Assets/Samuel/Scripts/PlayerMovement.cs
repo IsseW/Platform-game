2227d3865c4aa06821416a0127ac79117475b0b7
Samuel Möllerström <samuel.mollerstrom@learnet.se>
2019-09-11 12:25:30 +0200

    k

--- Assets/Samuel/Scripts/PlayerMovement.cs
+++ Assets/Samuel/Scripts/PlayerMovement.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         

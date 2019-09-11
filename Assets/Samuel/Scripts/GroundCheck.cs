77a28a8dbac4a4e3055f3570ab4942747ac9c76e
Samuel Möllerström <samuel.mollerstrom@learnet.se>
2019-09-11 14:00:07 +0200

    d

--- Assets/Samuel/Scripts/GroundCheck.cs
+++ Assets/Samuel/Scripts/GroundCheck.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class GroundCheck : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public PlayerMovement movement;
     
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            movement.isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            movement.isGrounded = false;
+        }
     }
 }

b3248cbbf5c7fb8fcbaedb54ff044c35bca6950e
Hytos <54358345+Hytos@users.noreply.github.com>
2019-09-18 12:09:06 +0200

    keybindings UI

--- Assets/Robin/Scripts/Keybindings.cs
+++ Assets/Robin/Scripts/Keybindings.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Keybindings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public Button BmoveLeft;
+    public Button BmoveRight;
+    public Button BJump;
+    public Button BAttack;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public void BmoveLeftClick()
+  {
+        BmoveLeft.SetEnabled(false);
+
+
+  }
+
+   
 }
